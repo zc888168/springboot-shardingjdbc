46f015a9faabbdbfd8538a0bc4e1f7a9b233197f
chaozhang17 <chaozhang17@iflytek.com>
2018-10-24 00:18:42 +0800

    commit

--- BootShardingStudy/src/main/java/com/think/conf/DataSourceConfig.java
+++ BootShardingStudy/src/main/java/com/think/conf/DataSourceConfig.java
@@ -2,6 +2,7 @@ package com.think.conf;
 
 import javax.sql.DataSource;
 
+import com.alibaba.druid.pool.DruidDataSource;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
@@ -22,13 +23,15 @@ public class DataSourceConfig {
     // 配置文件中前缀
     @ConfigurationProperties(prefix = "spring.datasource.primary")
     public DataSource primaryDataSource() {
-        return DataSourceBuilder.create().build();
+        DruidDataSource dataSource = new DruidDataSource();
+        return  dataSource;
     }
 
     @Bean(name = "secondaryDataSource")
     @ConfigurationProperties(prefix = "spring.datasource.secondary")
     public DataSource secondaryDataSource() {
-        return DataSourceBuilder.create().build();
+        DruidDataSource dataSource = new DruidDataSource();
+        return  dataSource;
     }
 
 }

bec042cab96e458e99cbb5aadb99b3490d50e947
chaozhang17 <chaozhang17@iflytek.com>
2018-10-23 16:41:03 +0800

    igonre

--- BootShardingStudy/src/main/java/com/think/conf/DataSourceConfig.java
+++ BootShardingStudy/src/main/java/com/think/conf/DataSourceConfig.java
@@ -1,12 +1,12 @@
 package com.think.conf;
 
-import javax.sql.DataSource;
-
-import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
+import com.alibaba.druid.pool.DruidDataSource;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+
+import javax.sql.DataSource;
 
 /***
  * 配置多个数据源
@@ -22,13 +22,25 @@ public class DataSourceConfig {
     // 配置文件中前缀
     @ConfigurationProperties(prefix = "spring.datasource.primary")
     public DataSource primaryDataSource() {
-        return DataSourceBuilder.create().build();
+       // return DataSourceBuilder.create().build();
+        DruidDataSource dataSource = new DruidDataSource();
+        dataSource.setUrl("jdbc:mysql://127.0.0.1:3306/test0");
+        dataSource.setUsername("root");// 用户名
+        dataSource.setPassword("root");// 密码
+        return dataSource;
     }
 
     @Bean(name = "secondaryDataSource")
     @ConfigurationProperties(prefix = "spring.datasource.secondary")
     public DataSource secondaryDataSource() {
-        return DataSourceBuilder.create().build();
+        // return DataSourceBuilder.create().build();
+        DruidDataSource dataSource = new DruidDataSource();
+        dataSource.setUrl("jdbc:mysql://127.0.0.1:3306/test1");
+        dataSource.setUsername("root");// 用户名
+        dataSource.setPassword("root");// 密码
+        return dataSource;
     }
+
+
 
 }

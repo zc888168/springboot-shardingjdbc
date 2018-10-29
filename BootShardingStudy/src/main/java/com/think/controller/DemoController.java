5238f32ae74bcdbad9df61c40aad2585a1242cc4
chaozhang17 <chaozhang17@iflytek.com>
2018-10-29 14:12:35 +0800

    add page and order

--- BootShardingStudy/src/main/java/com/think/controller/DemoController.java
+++ BootShardingStudy/src/main/java/com/think/controller/DemoController.java
@@ -1,12 +1,16 @@
 package com.think.controller;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
 import com.think.dao.OrderMapper;
 import com.think.entity.TOrder;
+import jdk.nashorn.internal.runtime.JSONFunctions;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
+import java.util.List;
 
 /**
  * Description:
@@ -44,6 +48,17 @@ public class DemoController {
 
         orderMapper.findIn();
         return " success" ;
+    }
+
+    @GetMapping("/order")
+    public Object testOrder(){
+       List<TOrder> list =  orderMapper.testOrder();
+       return JSONArray.toJSON(list);
+    }
+    @GetMapping("/testPage")
+    public Object testPage(){
+        List<TOrder> list =  orderMapper.testPage();
+        return JSONArray.toJSON(list);
     }
 
     @GetMapping("/listbetween")

2c43e5737b147c3bd93ad6b7ed0e6f9160948fac
chaozhang17 <chaozhang17@iflytek.com>
2018-11-22 22:22:31 +0800

    集成swagger-ui

--- BootShardingStudy/src/main/java/com/think/main/Application.java
+++ BootShardingStudy/src/main/java/com/think/main/Application.java
@@ -4,10 +4,12 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @ComponentScan("com.*.**")
 @MapperScan("com.think.dao")
+@EnableSwagger2
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);

fc9a808d041d1c7ce3e172b9a25e5ae27a001b44
chaozhang17 <chaozhang17@iflytek.com>
2018-11-22 22:25:32 +0800

    aa

--- BootShardingStudy/src/main/java/com/think/controller/DemoController.java
+++ BootShardingStudy/src/main/java/com/think/controller/DemoController.java
@@ -4,6 +4,8 @@ import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONArray;
 import com.think.dao.OrderMapper;
 import com.think.entity.TOrder;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import jdk.nashorn.internal.runtime.JSONFunctions;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,6 +18,7 @@ import java.util.List;
  * Description:
  * Created by chaoz2 on 2018/10/22.
  */
+@Api(tags = "Demo")
 @RequestMapping(path = "/demo")
 @RestController
 public class DemoController {
@@ -44,6 +47,7 @@ public class DemoController {
         return " order" ;
     }
     @GetMapping("/listin")
+    @ApiOperation(value = "in测试")
     public Object listin(){
 
         orderMapper.findIn();
@@ -51,17 +55,20 @@ public class DemoController {
     }
 
     @GetMapping("/order")
+    @ApiOperation("orderTest")
     public Object testOrder(){
        List<TOrder> list =  orderMapper.testOrder();
        return JSONArray.toJSON(list);
     }
     @GetMapping("/testPage")
+    @ApiOperation("pageTest")
     public Object testPage(){
         List<TOrder> list =  orderMapper.testPage();
         return JSONArray.toJSON(list);
     }
 
     @GetMapping("/listbetween")
+    @ApiOperation("betweenTest")
     public Object listbetween(){
 
         orderMapper.findbetween();
